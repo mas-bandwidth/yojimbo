95fd79eb04246a2bd0dd31b19a6917d9b35d38c9
Glenn Fiedler <glenn.fiedler@gmail.com>
2016-06-13 11:09:50 -0700

    work in progress replacing queue

--- yojimbo_queue.h
+++ yojimbo_queue.h
@@ -1,142 +1,93 @@
 /*
     Yojimbo Client/Server Network Library.
-
-    Copyright © 2016, The Network Protocol Company, Inc.
     
+    Copyright © 2016, The Network Protocol Company, Inc.
+
     All rights reserved.
 */
 
 #ifndef YOJIMBO_QUEUE_H
 #define YOJIMBO_QUEUE_H
 
-#include "yojimbo_array.h"
-#include "yojimbo_types.h"
+#include "yojimbo_config.h"
+#include <assert.h>
+#include <string.h>
 
 namespace yojimbo
 {
-    template<typename T> void queue_internal_increase_capacity( Queue<T> & q, uint32_t new_capacity )
+    template <typename T> class Queue
     {
-        uint32_t end = array_size( q.m_data );
-        array_resize( q.m_data, new_capacity );
-        if ( q.m_offset + q.m_size > end ) 
+        T * m_entries;
+        int m_arraySize;
+        int m_startIndex;
+        int m_numEntries;
+
+    public:
+
+        Queue( int size )
         {
-            uint32_t end_items = end - q.m_offset;
-            memmove( array_begin( q.m_data ) + new_capacity - end_items, array_begin( q.m_data ) + q.m_offset, end_items * sizeof(T) );
-            q.m_offset += new_capacity - end;
+            assert( size > 0 );
+            m_entries = new T[size];
+            m_arraySize = size;
+            m_startIndex = 0;
+            m_numEntries = 0;
         }
-    }
 
-    template<typename T> void queue_internal_grow( Queue<T> & q, uint32_t min_capacity = 0 )
-    {
-        uint32_t new_capacity = array_size( q.m_data ) * 2 + 8;
-        if ( new_capacity < min_capacity )
-            new_capacity = min_capacity;
-        queue_internal_increase_capacity( q, new_capacity );
-    }
+        ~Queue()
+        {
+            delete [] m_entries;
+            m_arraySize = 0;
+            m_startIndex = 0;
+            m_numEntries = 0;
+        }
 
-    template<typename T> inline uint32_t queue_size( const Queue<T> & q )
-    {
-        return q.m_size;
-    }
+        void Clear()
+        {
+            m_numEntries = 0;
+            m_startIndex = 0;
+        }
 
-    template<typename T> inline uint32_t queue_space( const Queue<T> & q )
-    {
-        return array_size( q.m_data ) - q.m_size;
-    }
+        T Pop()
+        {
+            assert( !IsEmpty() );
+            const T & entry = m_entries[m_startIndex];
+            m_startIndex = ( m_startIndex + 1 ) % m_arraySize;
+            m_numEntries--;
+            return entry;
+        }
 
-    template<typename T> void queue_reserve( Queue<T> & q, uint32_t size )
-    {
-        if ( size > q.m_size )
-            queue_internal_increase_capacity( q, size );
-    }
+        void Push( const T & entry )
+        {
+            assert( !IsFull() );
+            m_entries[m_startIndex+m_numEntries] = entry;
+            m_numEntries++;
+        }
 
-    template<typename T> inline void queue_push_back( Queue<T> & q, const T & item )
-    {
-        if ( !queue_space( q ) )
-            queue_internal_grow( q );
-        q[q.m_size++] = item;
-    }
+        T & operator [] ( int index )
+        {
+            return m_entries[ ( m_startIndex + index ) % m_arraySize ];
+        }
 
-    template<typename T> inline void queue_pop_back( Queue<T> & q )
-    {
-        --q.m_size;
-    }
-    
-    template<typename T> inline void queue_push_front( Queue<T> & q, const T & item )
-    {
-        if ( !space( q ) )
-            queue_internal_grow( q );
-        q.m_offset = ( q.m_offset - 1 + array_size( q.m_data ) ) % array_size( q.m_data );
-        ++q.m_size;
-        q[0] = item;
-    }
-    
-    template<typename T> inline void queue_pop_front( Queue<T> & q )
-    {
-        q.m_offset = ( q.m_offset + 1 ) % array_size( q.m_data );
-        --q.m_size;
-    }
+        const T & operator [] ( int index ) const
+        {
+            return m_entries[ ( m_startIndex + index ) % m_arraySize ];
+        }
 
-    template<typename T> inline void queue_consume( Queue<T> & q, uint32_t n )
-    {
-        q.m_offset = ( q.m_offset + n) % array_size( q.m_data );
-        q.m_size -= n;
-    }
+        int GetSize() const
+        {
+            return m_numEntries;
+        }
 
-    template<typename T> void queue_clear( Queue<T> & q )
-    {
-        queue_consume( q, queue_size(q) );
-    }
+        bool IsFull() const
+        {
+            return m_numEntries == m_arraySize;
+        }
 
-    template<typename T> void queue_push( Queue<T> & q, const T * items, uint32_t n )
-    {
-        if ( space(q) < n )
-            queue_internal_grow( q, size( q ) + n );
-        const uint32_t size = array_size( q.m_data );
-        const uint32_t insert = ( q.m_offset + q.m_size ) % size;
-        uint32_t to_insert = n;
-        if ( insert + to_insert > size )
-            to_insert = size - insert;
-        memcpy( array_begin(q.m_data) + insert, items, to_insert * sizeof(T) );
-        q.m_size += to_insert;
-        items += to_insert;
-        n -= to_insert;
-        memcpy( array_begin(q.m_data), items, n * sizeof(T) );
-        q.m_size += n;
-    }
-
-    template<typename T> inline T * queue_begin_front( Queue<T> & q )
-    {
-        return array_begin( q.m_data ) + q.m_offset;
-    }
-
-    template<typename T> inline const T * queue_begin_front( const Queue<T> & q )
-    {
-        return array_begin( q.m_data ) + q.m_offset;
-    }
-
-    template<typename T> T * queue_end_front( Queue<T> & q )
-    {
-        uint32_t end = q.m_offset + q.m_size;
-        return end > array_size( q.m_data ) ? array_end( q.m_data ) : array_begin( q.m_data ) + end;
-    }
-    template<typename T> const T * queue_end_front(const Queue<T> &q)
-    {
-        uint32_t end = q.m_offset + q.m_size;
-        return end > array_size( q.m_data ) ? array_end( q.m_data ) : array_begin( q.m_data ) + end;
-    }
-
-    template <typename T> inline Queue<T>::Queue( Allocator & allocator) : m_data( allocator ), m_size(0), m_offset(0) {}
-
-    template <typename T> inline T & Queue<T>::operator [] ( uint32_t i )
-    {
-        return m_data[ ( i + m_offset ) % array_size( m_data ) ];
-    }
-
-    template <typename T> inline const T & Queue<T>::operator [] ( uint32_t i ) const
-    {
-        return m_data[ ( i + m_offset ) % array_size( m_data ) ];
-    }
+        bool IsEmpty() const
+        {
+            return m_arraySize == 0;
+        }
+    };
 }
 
-#endif // #ifndef YOJIMBO_QUEUE
+#endif // #ifndef YOJIMBO_BITPACK_H
