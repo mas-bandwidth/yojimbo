d4180cffdd32ea9eae6ed32bbad5884e3737ac2f
Glenn Fiedler <glenn.fiedler@gmail.com>
2016-06-15 18:03:21 -0700

    removed all rights reserved notice

--- client_server.cpp
+++ client_server.cpp
@@ -2,8 +2,6 @@
     Client/Server Testbed
 
     Copyright © 2016, The Network Protocol Company, Inc.
-    
-    All rights reserved.
 
     Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
