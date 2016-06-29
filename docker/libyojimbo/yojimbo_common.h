2aba49388cb3bc7e04321c0a8e8164ceb2aa7d5a
Glenn Fiedler <Glenn Fiedler>
2016-06-28 23:51:45 -0700

    backport to windows

---
+++ docker/libyojimbo/yojimbo_common.h
@@ -0,0 +1,276 @@
+/*
+    Yojimbo Client/Server Network Library.
+
+    Copyright © 2016, The Network Protocol Company, Inc.
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+        1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+        2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+           in the documentation and/or other materials provided with the distribution.
+
+        3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived 
+           from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+    USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef YOJIMBO_COMMON_H
+#define YOJIMBO_COMMON_H
+
+#include "yojimbo_config.h"
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <assert.h>
+    
+namespace yojimbo
+{
+    inline int random_int( int a, int b )
+    {
+        assert( a < b );
+        int result = a + rand() % ( b - a + 1 );
+        assert( result >= a );
+        assert( result <= b );
+        return result;
+    }
+
+    inline float random_float( float a, float b )
+    {
+        assert( a < b );
+        float random = ( (float) rand() ) / (float) RAND_MAX;
+        float diff = b - a;
+        float r = random * diff;
+        return a + r;
+    }
+
+    #ifndef min
+    template <typename T> const T & min( const T & a, const T & b )
+    {
+        return ( a < b ) ? a : b;
+    }
+    #endif
+
+    #ifndef max
+    template <typename T> const T & max( const T & a, const T & b )
+    {
+        return ( a > b ) ? a : b;
+    }
+    #endif
+
+    template <typename T> T clamp( const T & value, const T & min, const T & max )
+    {
+        if ( value < min )
+            return min;
+        else if ( value > max )
+            return max;
+        else
+            return value;
+    }
+
+    template <typename T> void swap( T & a, T & b )
+    {
+        T tmp = a;
+        a = b;
+        b = tmp;
+    };
+
+    template <typename T> T abs( const T & value )
+    {
+        return ( value < 0 ) ? -value : value;
+    }
+
+    template <uint32_t x> struct PopCount
+    {
+        enum {   a = x - ( ( x >> 1 )       & 0x55555555 ),
+                 b =   ( ( ( a >> 2 )       & 0x33333333 ) + ( a & 0x33333333 ) ),
+                 c =   ( ( ( b >> 4 ) + b ) & 0x0f0f0f0f ),
+                 d =   c + ( c >> 8 ),
+                 e =   d + ( d >> 16 ),
+
+            result = e & 0x0000003f 
+        };
+    };
+
+    template <uint32_t x> struct Log2
+    {
+        enum {   a = x | ( x >> 1 ),
+                 b = a | ( a >> 2 ),
+                 c = b | ( b >> 4 ),
+                 d = c | ( c >> 8 ),
+                 e = d | ( d >> 16 ),
+                 f = e >> 1,
+
+            result = PopCount<f>::result
+        };
+    };
+
+    template <int64_t min, int64_t max> struct BitsRequired
+    {
+        static const uint32_t result = ( min == max ) ? 0 : ( Log2<uint32_t(max-min)>::result + 1 );
+    };
+
+    #define BITS_REQUIRED( min, max ) BitsRequired<min,max>::result
+
+    inline uint32_t popcount( uint32_t x )
+    {
+#ifdef __GNUC__
+        return __builtin_popcount( x );
+#else // #ifdef __GNUC__
+        const uint32_t a = x - ( ( x >> 1 )       & 0x55555555 );
+        const uint32_t b =   ( ( ( a >> 2 )       & 0x33333333 ) + ( a & 0x33333333 ) );
+        const uint32_t c =   ( ( ( b >> 4 ) + b ) & 0x0f0f0f0f );
+        const uint32_t d =   c + ( c >> 8 );
+        const uint32_t e =   d + ( d >> 16 );
+        const uint32_t result = e & 0x0000003f;
+        return result;
+#endif // #ifdef __GNUC__
+    }
+
+#ifdef __GNUC__
+
+    inline int bits_required( uint32_t min, uint32_t max )
+    {
+        return 32 - __builtin_clz( max - min );
+    }
+
+#else // #ifdef __GNUC__
+
+    inline uint32_t log2( uint32_t x )
+    {
+        const uint32_t a = x | ( x >> 1 );
+        const uint32_t b = a | ( a >> 2 );
+        const uint32_t c = b | ( b >> 4 );
+        const uint32_t d = c | ( c >> 8 );
+        const uint32_t e = d | ( d >> 16 );
+        const uint32_t f = e >> 1;
+        return popcount( f );
+    }
+
+    inline int bits_required( uint32_t min, uint32_t max )
+    {
+        return ( min == max ) ? 0 : log2( max - min ) + 1;
+    }
+
+#endif // #ifdef __GNUC__
+
+    inline uint32_t bswap( uint32_t value )
+    {
+#ifdef __GNUC__
+        return __builtin_bswap32( value );
+#else // #ifdef __GNUC__
+        return ( value & 0x000000ff ) << 24 | ( value & 0x0000ff00 ) << 8 | ( value & 0x00ff0000 ) >> 8 | ( value & 0xff000000 ) >> 24;
+#endif // #ifdef __GNUC__
+    }
+
+    inline uint16_t bswap( uint16_t value )
+    {
+        return ( value & 0x00ff ) << 8 | ( value & 0xff00 ) >> 8;
+    }
+
+    // IMPORTANT: These functions consider network order to be little endian because most modern processors are little endian. Least amount of work!
+
+    inline uint32_t host_to_network( uint32_t value )
+    {
+#if YOJIMBO_BIG_ENDIAN
+        return bswap( value );
+#else // #if YOJIMBO_BIG_ENDIAN
+        return value;
+#endif // #if YOJIMBO_BIG_ENDIAN
+    }
+
+    inline uint32_t network_to_host( uint32_t value )
+    {
+#if YOJIMBO_BIG_ENDIAN
+        return bswap( value );
+#else // #if YOJIMBO_BIG_ENDIAN
+        return value;
+#endif // #if YOJIMBO_BIG_ENDIAN
+    }
+
+    inline uint16_t host_to_network( uint16_t value )
+    {
+#if YOJIMBO_BIG_ENDIAN
+        return bswap( value );
+#else // #if YOJIMBO_BIG_ENDIAN
+        return value;
+#endif // #if YOJIMBO_BIG_ENDIAN
+    }
+
+    inline uint16_t network_to_host( uint16_t value )
+    {
+#if YOJIMBO_BIG_ENDIAN
+        return bswap( value );
+#else // #if YOJIMBO_BIG_ENDIAN
+        return value;
+#endif // #if YOJIMBO_BIG_ENDIAN
+    }
+
+    inline bool sequence_greater_than( uint16_t s1, uint16_t s2 )
+    {
+        return ( ( s1 > s2 ) && ( s1 - s2 <= 32768 ) ) || 
+               ( ( s1 < s2 ) && ( s2 - s1  > 32768 ) );
+    }
+
+    inline bool sequence_less_than( uint16_t s1, uint16_t s2 )
+    {
+        return sequence_greater_than( s2, s1 );
+    }
+
+    inline int sequence_difference( uint16_t _s1, uint16_t _s2 )
+    {
+        int s1 = _s1;
+        int s2 = _s2;
+        if ( abs( s1 - s2 ) >= 32786 )
+        {
+            if ( s1 > s2 )
+                s2 += 65536;
+            else
+                s1 += 65536;
+        }
+        return s1 - s2;
+    }
+
+    inline int signed_to_unsigned( int n )
+    {
+        return ( n << 1 ) ^ ( n >> 31 );
+    }
+
+    inline int unsigned_to_signed( uint32_t n )
+    {
+        return ( n >> 1 ) ^ ( -int32_t( n & 1 ) );
+    }
+
+    void compress_packet_sequence( uint64_t sequence, uint8_t & prefix_byte, int & num_sequence_bytes, uint8_t * sequence_bytes );
+
+    int get_packet_sequence_bytes( uint8_t prefix_byte );
+
+    uint64_t decompress_packet_sequence( uint8_t prefix_byte, const uint8_t * sequence_bytes );
+
+    uint32_t calculate_crc32( const uint8_t *buffer, size_t length, uint32_t crc32 = 0 );
+
+    uint32_t hash_data( const uint8_t * data, uint32_t length, uint32_t hash );
+
+    uint32_t hash_string( const char string[], uint32_t hash );
+
+    uint64_t murmur_hash_64( const void * key, uint32_t length, uint64_t seed );
+
+    void print_bytes( const char * label, const uint8_t * data, int data_bytes );
+
+    int base64_encode_string( const char * input, char * output, int output_size );
+
+    int base64_decode_string( const char * input, char * output, int output_size );
+
+    int base64_encode_data( const uint8_t * input, int input_length, char * output, int output_size );
+
+    int base64_decode_data( const char * input, uint8_t * output, int output_size );
+}
+
+#endif // #ifndef YOJIMBO_COMMON_H
---
+++ docker/libyojimbo/yojimbo_stream.h
@@ -0,0 +1,406 @@
+/*
+    Yojimbo Client/Server Network Library.
+    
+    Copyright © 2016, The Network Protocol Company, Inc.
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+        1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+        2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+           in the documentation and/or other materials provided with the distribution.
+
+        3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived 
+           from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+    USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef YOJIMBO_STREAM_H
+#define YOJIMBO_STREAM_H
+
+#include "yojimbo_config.h"
+#include "yojimbo_bitpack.h"
+#ifdef DEBUG
+#include <stdio.h>
+#endif // #ifdef DEBUG
+
+namespace yojimbo
+{
+    #define YOJIMBO_PROTOCOL_ERROR_NONE                         0
+    #define YOJIMBO_PROTOCOL_ERROR_CRC32_MISMATCH               1
+    #define YOJIMBO_PROTOCOL_ERROR_INVALID_PACKET_TYPE          2
+    #define YOJIMBO_PROTOCOL_ERROR_PACKET_TYPE_NOT_ALLOWED      3
+    #define YOJIMBO_PROTOCOL_ERROR_CREATE_PACKET_FAILED         4
+    #define YOJIMBO_PROTOCOL_ERROR_SERIALIZE_HEADER_FAILED      5
+    #define YOJIMBO_PROTOCOL_ERROR_SERIALIZE_PACKET_FAILED      6
+    #define YOJIMBO_PROTOCOL_ERROR_SERIALIZE_CHECK_FAILED       7
+    #define YOJIMBO_PROTOCOL_ERROR_STREAM_OVERFLOW              8
+    #define YOJIMBO_PROTOCOL_ERROR_STREAM_ABORTED               9
+
+    class WriteStream
+    {
+    public:
+
+        enum { IsWriting = 1 };
+        enum { IsReading = 0 };
+
+        WriteStream( uint8_t * buffer, int bytes ) : m_error( YOJIMBO_PROTOCOL_ERROR_NONE ), m_context( NULL ), m_writer( buffer, bytes ) {}
+
+        bool SerializeInteger( int32_t value, int32_t min, int32_t max )
+        {
+            assert( min < max );
+            assert( value >= min );
+            assert( value <= max );
+            const int bits = bits_required( min, max );
+            uint32_t unsigned_value = value - min;
+            m_writer.WriteBits( unsigned_value, bits );
+            return true;
+        }
+
+        bool SerializeBits( uint32_t value, int bits )
+        {
+            assert( bits > 0 );
+            assert( bits <= 32 );
+            m_writer.WriteBits( value, bits );
+            return true;
+        }
+
+        bool SerializeBytes( const uint8_t * data, int bytes )
+        {
+            assert( data );
+            assert( bytes >= 0 );
+            if ( !SerializeAlign() )
+                return false;
+            m_writer.WriteBytes( data, bytes );
+            return true;
+        }
+
+        bool SerializeAlign()
+        {
+            m_writer.WriteAlign();
+            return true;
+        }
+
+        int GetAlignBits() const
+        {
+            return m_writer.GetAlignBits();
+        }
+
+        bool SerializeCheck( const char * string )
+        {
+#if YOJIMBO_SERIALIZE_CHECKS
+            SerializeAlign();
+            const uint32_t magic = hash_string( string, 0 );
+            SerializeBits( magic, 32 );
+#endif // #if YOJIMBO_SERIALIZE_CHECKS
+            return true;
+        }
+
+        void Flush()
+        {
+            m_writer.FlushBits();
+        }
+
+        const uint8_t * GetData() const
+        {
+            return m_writer.GetData();
+        }
+
+        int GetBytesProcessed() const
+        {
+            return m_writer.GetBytesWritten();
+        }
+
+        int GetBitsProcessed() const
+        {
+            return m_writer.GetBitsWritten();
+        }
+
+        int GetBitsRemaining() const
+        {
+            return GetTotalBits() - GetBitsProcessed();
+        }
+
+        int GetTotalBits() const
+        {
+            return m_writer.GetTotalBytes() * 8;
+        }
+
+        int GetTotalBytes() const
+        {
+            return m_writer.GetTotalBytes();
+        }
+
+        void SetContext( void *context )
+        {
+            m_context = context;
+        }
+
+        void * GetContext() const
+        {
+            return m_context;
+        }
+
+        int GetError() const
+        {
+            return m_error;
+        }
+
+    private:
+
+        int m_error;
+        void *m_context;
+        BitWriter m_writer;
+    };
+
+    class ReadStream
+    {
+    public:
+
+        enum { IsWriting = 0 };
+        enum { IsReading = 1 };
+
+        ReadStream( const uint8_t * buffer, int bytes ) : m_context( NULL ), m_error( YOJIMBO_PROTOCOL_ERROR_NONE ), m_bitsRead(0), m_reader( buffer, bytes ) {}
+
+        bool SerializeInteger( int32_t & value, int32_t min, int32_t max )
+        {
+            assert( min < max );
+            const int bits = bits_required( min, max );
+            if ( m_reader.WouldOverflow( bits ) )
+            {
+                m_error = YOJIMBO_PROTOCOL_ERROR_STREAM_OVERFLOW;
+                return false;
+            }
+            uint32_t unsigned_value = m_reader.ReadBits( bits );
+            value = (int32_t) unsigned_value + min;
+            m_bitsRead += bits;
+            return true;
+        }
+
+        bool SerializeBits( uint32_t & value, int bits )
+        {
+            assert( bits > 0 );
+            assert( bits <= 32 );
+            if ( m_reader.WouldOverflow( bits ) )
+            {
+                m_error = YOJIMBO_PROTOCOL_ERROR_STREAM_OVERFLOW;
+                return false;
+            }
+            uint32_t read_value = m_reader.ReadBits( bits );
+            value = read_value;
+            m_bitsRead += bits;
+            return true;
+        }
+
+        bool SerializeBytes( uint8_t * data, int bytes )
+        {
+            if ( !SerializeAlign() )
+                return false;
+            if ( m_reader.WouldOverflow( bytes * 8 ) )
+            {
+                m_error = YOJIMBO_PROTOCOL_ERROR_STREAM_OVERFLOW;
+                return false;
+            }
+            m_reader.ReadBytes( data, bytes );
+            m_bitsRead += bytes * 8;
+            return true;
+        }
+
+        bool SerializeAlign()
+        {
+            const int alignBits = m_reader.GetAlignBits();
+            if ( m_reader.WouldOverflow( alignBits ) )
+            {
+                m_error = YOJIMBO_PROTOCOL_ERROR_STREAM_OVERFLOW;
+                return false;
+            }
+            if ( !m_reader.ReadAlign() )
+                return false;
+            m_bitsRead += alignBits;
+            return true;
+        }
+
+        int GetAlignBits() const
+        {
+            return m_reader.GetAlignBits();
+        }
+
+        bool SerializeCheck( const char * string )
+        {
+#if YOJIMBO_SERIALIZE_CHECKS            
+            SerializeAlign();
+            uint32_t value = 0;
+            SerializeAlign();
+            SerializeBits( value, 32 );
+            const uint32_t magic = hash_string( string, 0 );
+#ifdef DEBUG
+            if ( magic != value )
+            {
+                printf( "serialize check failed: '%s'. expected %x, got %x\n", string, magic, value );
+            }
+#endif // #ifdef DEBUG
+            return value == magic;
+#else // #if YOJIMBO_SERIALIZE_CHECKS
+            return true;
+#endif // #if YOJIMBO_SERIALIZE_CHECKS
+        }
+
+        int GetBitsProcessed() const
+        {
+            return m_bitsRead;
+        }
+
+        int GetBitsRemaining() const
+        {
+            return m_reader.GetBitsRemaining();
+        }
+
+        int GetBytesProcessed() const
+        {
+            return ( m_bitsRead + 7 ) / 8;
+        }
+
+        void SetContext( void * context )
+        {
+            m_context = context;
+        }
+
+        void * GetContext() const
+        {
+            return m_context;
+        }
+
+        int GetError() const
+        {
+            return m_error;
+        }
+
+        int GetBytesRead() const
+        {
+            return m_reader.GetBytesRead();
+        }
+
+    private:
+
+        void * m_context;
+        int m_error;
+        int m_bitsRead;
+        BitReader m_reader;
+    };
+
+    class MeasureStream
+    {
+    public:
+
+        enum { IsWriting = 1 };
+        enum { IsReading = 0 };
+
+        MeasureStream( int bytes ) : m_context( NULL ), m_error( YOJIMBO_PROTOCOL_ERROR_NONE ), m_totalBytes( bytes ), m_bitsWritten(0) {}
+
+#ifdef DEBUG
+        bool SerializeInteger( int32_t value, int32_t min, int32_t max )
+#else // #ifdef DEBUG
+        bool SerializeInteger( int32_t /*value*/, int32_t min, int32_t max )
+#endif // #ifdef DEBUG
+        {
+            assert( min < max );
+            assert( value >= min );
+            assert( value <= max );
+            const int bits = bits_required( min, max );
+            m_bitsWritten += bits;
+            return true;
+        }
+
+        bool SerializeBits( uint32_t /*value*/, int bits )
+        {
+            assert( bits > 0 );
+            assert( bits <= 32 );
+            m_bitsWritten += bits;
+            return true;
+        }
+
+        bool SerializeBytes( const uint8_t * /*data*/, int bytes )
+        {
+            SerializeAlign();
+            m_bitsWritten += bytes * 8;
+            return true;
+        }
+
+        bool SerializeAlign()
+        {
+            const int alignBits = GetAlignBits();
+            m_bitsWritten += alignBits;
+            return true;
+        }
+
+        int GetAlignBits() const
+        {
+            return 7;       // we can't know for sure, so be conservative and assume worst case
+        }
+
+        bool SerializeCheck( const char * /*string*/ )
+        {
+#if YOJIMBO_SERIALIZE_CHECKS
+            SerializeAlign();
+            m_bitsWritten += 32;
+#endif // #if YOJIMBO_SERIALIZE_CHECKS
+            return true;
+        }
+
+        int GetBitsProcessed() const
+        {
+            return m_bitsWritten;
+        }
+
+        int GetBitsRemaining() const
+        {
+            return m_totalBytes * 8 - GetBitsProcessed();
+        }
+
+        int GetBytesProcessed() const
+        {
+            return ( m_bitsWritten + 7 ) / 8;
+        }
+
+        int GetTotalBytes() const
+        {
+            return m_totalBytes;
+        }
+
+        int GetTotalBits() const
+        {
+            return m_totalBytes * 8;
+        }
+
+        void SetContext( void * context )
+        {
+            m_context = context;
+        }
+
+        void * GetContext() const
+        {
+            return m_context;
+        }
+
+        int GetError() const
+        {
+            return m_error;
+        }
+
+    private:
+
+        void * m_context;
+        int m_error;
+        int m_totalBytes;
+        int m_bitsWritten;
+    };
+}
+
+#endif // #ifndef YOJIMBO_STREAM_H
