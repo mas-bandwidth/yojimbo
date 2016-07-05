6ed460d5bcbbd0f39dc78a78933f7e180f772772
Glenn Fiedler <glenn.fiedler@gmail.com>
2016-07-05 00:19:34 -0700

    added soak test

---
+++ soak.cpp
@@ -0,0 +1,247 @@
+/*
+    Client/Server Testbed
+
+    Copyright © 2016, The Network Protocol Company, Inc.
+
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+        1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+        2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+           in the documentation and/or other materials provided with the distribution.
+
+        3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived 
+           from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+    WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+    USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#define SERVER 1
+#define CLIENT 1
+#define MATCHER 1
+
+#include "shared.h"
+#include <signal.h>
+
+static volatile int quit = 0;
+
+void interrupt_handler( int /*dummy*/ )
+{
+    quit = 1;
+}
+
+int ClientServerMain()
+{
+    LocalMatcher matcher;
+
+    uint64_t clientId = 1;
+
+    uint8_t connectTokenData[ConnectTokenBytes];
+    uint8_t connectTokenNonce[NonceBytes];
+
+    uint8_t clientToServerKey[KeyBytes];
+    uint8_t serverToClientKey[KeyBytes];
+
+    int numServerAddresses;
+    Address serverAddresses[MaxServersPerConnectToken];
+
+    memset( connectTokenNonce, 0, NonceBytes );
+
+    if ( !matcher.RequestMatch( clientId, connectTokenData, connectTokenNonce, clientToServerKey, serverToClientKey, numServerAddresses, serverAddresses ) )
+    {
+        printf( "error: request match failed\n" );
+        return 1;
+    }
+
+    GamePacketFactory packetFactory;
+
+    NetworkSimulator networkSimulator( GetDefaultAllocator() );
+
+    Address clientAddress( "::1", ClientPort );
+    Address serverAddress( "::1", ServerPort );
+
+    SimulatorInterface clientInterface( GetDefaultAllocator(), networkSimulator, packetFactory, clientAddress, ProtocolId );
+    SimulatorInterface serverInterface( GetDefaultAllocator(), networkSimulator, packetFactory, serverAddress, ProtocolId );
+
+    TestMessageFactory messageFactory( GetDefaultAllocator() );
+
+    ConnectionContext context;
+    context.messageFactory = &messageFactory;
+
+    clientInterface.SetContext( &context );
+    serverInterface.SetContext( &context );
+
+    double time = 0.0;
+
+    GameClient client( GetDefaultAllocator(), clientInterface, &messageFactory );
+
+    GameServer server( GetDefaultAllocator(), serverInterface, &messageFactory );
+
+    server.SetServerAddress( serverAddress );
+
+    server.Start();
+    
+    client.Connect( serverAddress, connectTokenData, connectTokenNonce, clientToServerKey, serverToClientKey );
+
+    uint64_t numMessagesSentToServer = 0;
+    uint64_t numMessagesReceivedFromClient = 0;
+
+    signal( SIGINT, interrupt_handler );    
+
+    while ( !quit )
+    {
+        if ( ( rand() % 10000 ) == 0 )
+        {
+            switch ( rand() % 5 )
+            {
+                case 0:
+                    networkSimulator.SetJitter( 0 );
+                    networkSimulator.SetLatency( 0 );
+                    networkSimulator.SetDuplicates( 0 );
+                    networkSimulator.SetPacketLoss( 0 );
+                    break;
+
+                case 1:
+                    networkSimulator.SetJitter( 50 );
+                    networkSimulator.SetLatency( 50 );
+                    networkSimulator.SetDuplicates( 1 );
+                    networkSimulator.SetPacketLoss( 2 );
+                    break;
+
+                case 2:
+                    networkSimulator.SetJitter( 250 );
+                    networkSimulator.SetLatency( 250 );
+                    networkSimulator.SetDuplicates( 10 );
+                    networkSimulator.SetPacketLoss( 25 );
+                    break;
+
+                case 3:
+                    networkSimulator.SetJitter( 250 );
+                    networkSimulator.SetLatency( 1000 );
+                    networkSimulator.SetDuplicates( 90 );
+                    networkSimulator.SetPacketLoss( 50 );
+                    break;
+
+                default:
+                    networkSimulator.SetJitter( 250 );
+                    networkSimulator.SetLatency( 1000 );
+                    networkSimulator.SetDuplicates( 90 );
+                    networkSimulator.SetPacketLoss( 90 );
+                    break;
+            }
+        }
+
+        client.SendPackets();
+        server.SendPackets();
+
+        clientInterface.WritePackets();
+        serverInterface.WritePackets();
+
+        clientInterface.ReadPackets();
+        serverInterface.ReadPackets();
+
+        client.ReceivePackets();
+        server.ReceivePackets();
+
+        client.CheckForTimeOut();
+        server.CheckForTimeOut();
+
+        if ( client.ConnectionFailed() )
+        {
+            printf( "error: client connect failed!\n" );
+            break;
+        }
+
+        time += 0.1f;
+
+        if ( client.IsConnected() )
+        {
+            const int messagesToSend = random_int( 0, 32 );
+
+            for ( int i = 0; i < messagesToSend; ++i )
+            {
+                if ( !client.CanSendMessage() )
+                    break;
+
+                TestMessage * message = (TestMessage*) messageFactory.Create( MESSAGE_TEST );
+                
+                if ( message )
+                {
+                    message->sequence = numMessagesSentToServer;
+                    
+                    printf( "client sent message %d\n", message->sequence );
+
+                    client.SendMessage( message );
+
+                    numMessagesSentToServer++;
+                }
+            }
+        
+            const int clientIndex = client.GetClientIndex();
+
+            while ( true )
+            {
+                Message * message = server.ReceiveMessage( clientIndex );
+
+                if ( !message )
+                    break;
+
+                assert( message->GetId() == (uint16_t) numMessagesReceivedFromClient );
+                assert( message->GetType() == MESSAGE_TEST );
+
+                TestMessage * testMessage = (TestMessage*) message;
+
+                assert( testMessage->sequence == uint16_t( numMessagesReceivedFromClient ) );
+
+                printf( "server received message %d\n", testMessage->sequence );
+
+                server.ReleaseMessage( message );
+
+                numMessagesReceivedFromClient++;
+            }
+
+        }
+
+        client.AdvanceTime( time );
+        server.AdvanceTime( time );
+
+        clientInterface.AdvanceTime( time );
+        serverInterface.AdvanceTime( time );
+    }
+
+    if ( quit )
+    {
+        printf( "\nserver stopped\n" );
+    }
+
+    return 0;
+}
+
+int main()
+{
+    printf( "\n" );
+
+    verbose_logging = true;
+
+    if ( !InitializeYojimbo() )
+    {
+        printf( "error: failed to initialize Yojimbo!\n" );
+        return 1;
+    }
+
+    srand( (unsigned int) time( NULL ) );
+
+    int result = ClientServerMain();
+
+    ShutdownYojimbo();
+
+    printf( "\n" );
+
+    return result;
+}
