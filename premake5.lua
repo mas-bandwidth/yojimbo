f9f298845b97959317b7d190cad1a90c47c130f5
Glenn Fiedler <Glenn Fiedler>
2016-07-12 20:22:17 -0700

    windows profiling

--- premake5.lua
+++ premake5.lua
@@ -21,16 +21,12 @@ solution "Yojimbo"
         targetdir "bin/"  
     end
     configurations { "Debug", "Release" }
-    -- temporary: need symbols in release for profiling
---    flags { "ExtraWarnings", "FatalWarnings", "StaticRuntime", "FloatFast" }
-    flags { "ExtraWarnings", "StaticRuntime", "FloatFast" }
+    flags { "ExtraWarnings", "FatalWarnings", "StaticRuntime", "FloatFast" }
     rtti "Off"
     configuration "Debug"
         flags { "Symbols" }
         defines { "DEBUG" }
     configuration "Release"
-    -- for profiling
-        flags { "Symbols" }
         optimize "Speed"
         defines { "NDEBUG" }
 
