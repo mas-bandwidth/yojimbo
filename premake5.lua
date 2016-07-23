d8488c536d96025a6299250cbf0536574c55fb10
Glenn Fiedler <glenn.fiedler@gmail.com>
2016-07-22 23:36:39 -0700

    prep for release 8

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,5 @@
 
-libyojimbo_version = "0.3.0-Preview7"
+libyojimbo_version = "0.3.0-Preview8"
 
 if os.is "windows" then
     debug_libs = { "sodium-debug", "mbedtls-debug", "mbedx509-debug", "mbedcrypto-debug" }
