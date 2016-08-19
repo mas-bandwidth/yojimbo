2d480b3b90519d9f6ccec380c3c97d2358910f77
Glenn Fiedler <Glenn Fiedler>
2016-08-19 12:36:49 -0700

    fix broken docker copy files step on windows

--- docker/copyFiles.bat
+++ docker/copyFiles.bat
@@ -8,3 +8,5 @@ copy ..\tests\*.* libyojimbo\tests
 copy ..\premake5.lua libyojimbo
 robocopy /MIR /DCOPY:T ..\tlsf libyojimbo\tlsf
 robocopy /MIR /DCOPY:T ..\rapidjson libyojimbo\rapidjson
+REM because robocopy returns non-zero error codes on successful operation. what the actual fuck windows
+cmd /c "exit /b 0"
