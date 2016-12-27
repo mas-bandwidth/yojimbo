03b25557eb39f1632882c9ea301f0c34ba004a77
gafferongames <glenn.fiedler@gmail.com>
2016-12-27 14:08:23 -0800

    documented yojimbo_platform.h

--- yojimbo_platform.h
+++ yojimbo_platform.h
@@ -29,9 +29,21 @@
 
 namespace yojimbo
 {
-    // todo: document this file
+    /**
+        Sleep for approximately this number of seconds.
+
+        @param time number of seconds to sleep for.
+     */
 
     void platform_sleep( double time );
+
+    /**
+        Get a high precision time value in seconds since the application has started.
+
+        IMPORTANT: Please store time in doubles so you retain sufficient precision as time increases.
+
+        @returns The current time value in seconds since the program started.
+     */
 
     double platform_time();
 }
