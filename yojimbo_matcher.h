1034908e3f7a82b520c245288f892bf5e17af69f
gafferongames <glenn.fiedler@gmail.com>
2016-12-27 14:43:58 -0800

    partial documentation of yojimbo_matcher.h

--- yojimbo_matcher.h
+++ yojimbo_matcher.h
@@ -31,15 +31,28 @@
 
 namespace yojimbo
 {
-    // todo: document this file
+    /**
+        Matcher status enum.
+
+        Designed for when the matcher will be made non-blocking. Currently the matcher is blocking.
+     */
 
     enum MatcherStatus
     {
-        MATCHER_IDLE,
-        MATCHER_BUSY,
-        MATCHER_READY,
-        MATCHER_FAILED
+        MATCHER_IDLE,                                                       ///< The matcher is idle.
+        MATCHER_BUSY,                                                       ///< The matcher is busy requesting a match.
+        MATCHER_READY,                                                      ///< The matcher is finished requesting a match. The match response is ready to read with Matcher::GetMatchResponse.
+        MATCHER_FAILED                                                      ///< The matcher failed to find a match.
     };
+
+    /** 
+        Match response sent back from matcher for match request.
+
+        IMPORTANT: This response is transmitted over HTTPS because it contains encryption keys for packets sent between the client and server.
+
+        @see Client::Connect
+        @see ConnectToken
+     */
 
     struct MatchResponse
     {
@@ -53,14 +66,22 @@ namespace yojimbo
             connectTokenExpireTimestamp = 0;
         }
 
-        int numServerAddresses;
-        Address serverAddresses[MaxServersPerConnect];
-        uint8_t connectTokenData[ConnectTokenBytes];
-        uint8_t connectTokenNonce[NonceBytes];
-        uint8_t clientToServerKey[KeyBytes];
-        uint8_t serverToClientKey[KeyBytes];
-        uint64_t connectTokenExpireTimestamp;
+        int numServerAddresses;                                             ///< The number of server addresses to connect to in [1,MaxServersPerConnect].
+        Address serverAddresses[MaxServersPerConnect];                      ///< The array of server addresses that the client can connect to, in order of first to last to try.
+        uint8_t connectTokenData[ConnectTokenBytes];                        ///< The encrypted connect token data.
+        uint8_t connectTokenNonce[NonceBytes];                              ///< The nonce the connect token was encrypted with.
+        uint8_t clientToServerKey[KeyBytes];                                ///< The key for client to server encrypted packets.
+        uint8_t serverToClientKey[KeyBytes];                                ///< The key for server to client encrypted packets.
+        uint64_t connectTokenExpireTimestamp;                               ///< The timestamp at which this connect token expires.
     };
+
+    /**
+        The matcher class.
+
+        This is a stub class for HTTPS requests to matcher.go.
+
+        It will be improved in the future, most importantly to make Matcher::RequestMatch a non-blocking operation.
+     */
 
     class Matcher
     {
