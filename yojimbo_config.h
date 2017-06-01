04a395c0cc021635257f5be1ea208018037c5452
Glenn Fiedler <glenn.fiedler@gmail.com>
2017-05-31 17:54:32 -0700

    made 0.5.0 release

--- yojimbo_config.h
+++ yojimbo_config.h
@@ -32,7 +32,7 @@
 #endif
 
 #define YOJIMBO_MAJOR_VERSION 0
-#define YOJIMBO_MINOR_VERSION 4
+#define YOJIMBO_MINOR_VERSION 5
 #define YOJIMBO_PATCH_VERSION 0
 
 // Endianness detection from rapidjson
