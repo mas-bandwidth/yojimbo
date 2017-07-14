42f21957f81147a9a4147787ef6d630395cdc395
Glenn Fiedler <glenn.fiedler@gmail.com>
2017-07-14 10:39:07 -0700

    additional documentation

--- yojimbo_platform.h
+++ yojimbo_platform.h
@@ -104,18 +104,45 @@ void yojimbo_sleep( double time );
 
 double yojimbo_time();
 
-// todo: document this
-
 #define YOJIMBO_LOG_LEVEL_NONE      0
 #define YOJIMBO_LOG_LEVEL_ERROR     1
 #define YOJIMBO_LOG_LEVEL_INFO      2
 #define YOJIMBO_LOG_LEVEL_DEBUG     3
 
+/**
+    Set the yojimbo log level.
+
+    Valid log levels are:
+
+        YOJIMBO_LOG_LEVEL_NONE
+        YOJIMBO_LOG_LEVEL_ERROR
+        YOJIMBO_LOG_LEVEL_INFO
+        YOJIMBO_LOG_LEVEL_DEBUG
+
+    @param level The log level to set. Initially set to YOJIMBO_LOG_LEVEL_NONE.
+ */
+
 void yojimbo_log_level( int level );
+
+/**
+    Printf function used by yojimbo to emit logs.
+
+    This function internally calls the printf callback set by the user. 
+
+    @see yojimbo_set_printf_function
+ */
 
 void yojimbo_printf( int level, const char * format, ... );
 
 extern void (*yojimbo_assert_function)( const char *, const char *, const char * file, int line );
+
+/**
+    Assert function used by yojimbo.
+
+    This assert function lets the user override the assert presentation.
+
+    @see yojimbo_set_assert_functio
+ */
 
 #ifndef NDEBUG
 #define yojimbo_assert( condition )                                                         \
@@ -131,7 +158,19 @@ do                                                                              
 #define yojimbo_assert( ignore ) ((void)0)
 #endif
 
+/**
+    Call this to set the printf function to use for logging.
+
+    @param function The printf callback function.
+ */
+
 void yojimbo_set_printf_function( int (*function)( const char * /*format*/, ... ) );
+
+/**
+    Call this to set the function to call when an assert triggers.
+
+    @param function The assert callback function.
+ */
 
 void yojimbo_set_assert_function( void (*function)( const char * /*condition*/, const char * /*function*/, const char * /*file*/, int /*line*/ ) );
 
