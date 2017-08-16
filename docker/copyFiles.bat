e47edb4864c4b8baa73546188016cbe73f360d4a
Glenn Fiedler <glenn.fiedler@gmail.com>
2017-08-16 09:08:07 -0700

    fix docker action on windows

--- docker/copyFiles.bat
+++ docker/copyFiles.bat
@@ -4,7 +4,7 @@ mkdir yojimbo
 mkdir yojimbo\tests
 copy ..\*.h yojimbo
 copy ..\*.cpp yojimbo
-copy ..\premake5.lua libyojimbo
+copy ..\premake5.lua yojimbo
 robocopy /MIR /DCOPY:T ..\tlsf yojimbo\tlsf
 REM because robocopy sometimes sets non-zero error codes on successful operation. what the actual fuck windows
 cmd /c "exit /b 0"
