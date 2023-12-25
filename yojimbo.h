747613f9b3bcc07a16c02647eae5b6f1e9eb4d60
Glenn Fiedler <glenn@networknext.com>
2023-12-25 13:18:36 -0500

    moar

--- yojimbo.h
+++ yojimbo.h
@@ -38,6 +38,7 @@
 #include "yojimbo_channel.h"
 #include "yojimbo_reliable_ordered_channel.h"
 #include "yojimbo_unreliable_unordered_channel.h"
+#include "yojimbo_connection.h"
 
 /** @file */
 
@@ -77,60 +78,6 @@ void ShutdownYojimbo();
 
 namespace yojimbo
 {
-    /// Connection error level.
-
-    enum ConnectionErrorLevel
-    {
-        CONNECTION_ERROR_NONE = 0,                              ///< No error. All is well.
-        CONNECTION_ERROR_CHANNEL,                               ///< A channel is in an error state.
-        CONNECTION_ERROR_ALLOCATOR,                             ///< The allocator is an error state.
-        CONNECTION_ERROR_MESSAGE_FACTORY,                       ///< The message factory is in an error state.
-        CONNECTION_ERROR_READ_PACKET_FAILED,                    ///< Failed to read packet. Received an invalid packet?
-    };
-
-    /**
-        Sends and receives messages across a set of user defined channels.
-     */
-
-    class Connection
-    {
-    public:
-
-        Connection( Allocator & allocator, MessageFactory & messageFactory, const ConnectionConfig & connectionConfig, double time );
-
-        ~Connection();
-
-        void Reset();
-
-        bool CanSendMessage( int channelIndex ) const;
-
-        bool HasMessagesToSend( int channelIndex ) const;
-
-        void SendMessage( int channelIndex, Message * message, void *context = 0);
-
-        Message * ReceiveMessage( int channelIndex );
-
-        void ReleaseMessage( Message * message );
-
-        bool GeneratePacket( void * context, uint16_t packetSequence, uint8_t * packetData, int maxPacketBytes, int & packetBytes );
-
-        bool ProcessPacket( void * context, uint16_t packetSequence, const uint8_t * packetData, int packetBytes );
-
-        void ProcessAcks( const uint16_t * acks, int numAcks );
-
-        void AdvanceTime( double time );
-
-        ConnectionErrorLevel GetErrorLevel() { return m_errorLevel; }
-
-    private:
-
-        Allocator * m_allocator;                                ///< Allocator passed in to the connection constructor.
-        MessageFactory * m_messageFactory;                      ///< Message factory for creating and destroying messages.
-        ConnectionConfig m_connectionConfig;                    ///< Connection configuration.
-        Channel * m_channel[MaxChannels];                       ///< Array of connection channels. Array size corresponds to m_connectionConfig.numChannels
-        ConnectionErrorLevel m_errorLevel;                      ///< The connection error level.
-    };
-
     /**
         Simulates packet loss, latency, jitter and duplicate packets.
         This is useful during development, so your game is tested and played under real world conditions, instead of ideal LAN conditions.
