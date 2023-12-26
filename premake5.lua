c6870e19e8aa8d482f4865d1423f367d1f6a6cf8
Glenn Fiedler <glenn@networknext.com>
2023-12-26 10:25:22 -0500

    should fix linux

--- premake5.lua
+++ premake5.lua
@@ -36,6 +36,7 @@ project "sodium"
 
 project "netcode"
     kind "StaticLib"
+    links ["sodium"]
     language "C"
     defines { "NETCODE_ENABLE_TESTS=1" }
     files { "netcode/netcode.c", "netcode/netcode.h" }
